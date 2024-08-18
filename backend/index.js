b2171b9cb41ce5d569d08018a0267cbd623109b2
Ayush Garg <ayushkumargarg848@gmail.com>
2024-08-18 16:29:44 +0530

    Ready for Deployment

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.get('/', function(req, res){
 });
 
 // PORT
-const PORT = process.env.PORT || 8081;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
