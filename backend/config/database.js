a15e483e50a885291cf445855f458c3a942ec448
Ayush Garg <ayushkumargarg848@gmail.com>
2024-08-18 18:16:26 +0530

    Final Changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2";
 
-// create the connection to database
+import dotenv from "dotenv"; // Import dotenv
+dotenv.config(); // Load environment variables
+
+// Create the connection to database
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME
 });
 
