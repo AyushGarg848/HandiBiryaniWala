a3612e58455189386fd5ebd78073dfb2d2c05bf7
Ayush Garg <ayushkumargarg848@gmail.com>
2024-08-18 16:58:04 +0530

    Final Changes

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.get('/', function(req, res){
 });
 
 // PORT
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
