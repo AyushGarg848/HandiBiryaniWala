36a30b5e068bd31f6ac308c14323b6ad89251c43
Ayush Garg <ayushkumargarg848@gmail.com>
2024-08-18 16:49:01 +0530

    Deployment Ready

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.get('/', function(req, res){
 });
 
 // PORT
-const PORT = 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,5 +3,7 @@ import axios from "axios";
 window.axios = axios
 axios.defaults.withCredentials = false
 
-let backendUrl = "http://" + window.location.hostname.toString() + ":8081/api"
+//let backendUrl = "http://" + window.location.hostname.toString() + ":8081/api"
+let backendUrl = "https://handibiryaniwala.onrender.com/api"
+
 axios.defaults.baseURL = backendUrl
