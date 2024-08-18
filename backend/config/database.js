8e6b14adbd6c46f7ff3214cf74dc6d39bbc14175
Ayush Garg <ayushkumargarg848@gmail.com>
2024-08-18 22:58:59 +0530

    Final Changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 3306,
     database: process.env.DB_NAME
 });
 
